dc0a26bac68b5688cc402bf3018ef188ada3206a
Jose Alfaro Quiros <b30210@ecci.ucr.ac.cr>
2018-08-29 10:33:34 -0600

    Primer commit

---
+++ src/Controller/ViewEnrollmentsController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * ViewEnrollments Controller
+ *
+ * @property \App\Model\Table\ViewEnrollmentsTable $ViewEnrollments
+ *
+ * @method \App\Model\Entity\ViewEnrollment[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class ViewEnrollmentsController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $this->paginate = [
+            'contain' => ['Enrollments', 'Students', 'Subjects']
+        ];
+        $viewEnrollments = $this->paginate($this->ViewEnrollments);
+
+        $this->set(compact('viewEnrollments'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id View Enrollment id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $ViEnTable=$this->loadmodel('ViewEnrollments');
+        $viewEnrollment = $ViEnTable->getViewEnrollment($id);
+        $this->set('viewEnrollment', $viewEnrollment);
+    }
+}
